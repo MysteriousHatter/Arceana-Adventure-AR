c43e1b71c30de5546f436fc4eaaa86c4a11bb02e
Abu Bakarr Bangurah <animevideogamer@gmail.com>
2020-02-09 02:38:41 -0500

    2/9
    With added History Scene

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardView : MonoBehaviour
+{
+    public TextMeshProUGUI title;
+    public TextMeshProUGUI Upright;
+    public TextMeshProUGUI Reversed;
+    public Image Card_Image;
+
+    
+    public Cards[] card_list;
+
+    public void OpenCards(int card)
+    {
+        title.text = card_list[card].cName;
+        Upright.text = card_list[card].upright;
+        Reversed.text = card_list[card].downright;
+        Card_Image.sprite = card_list[card].CardImg;
+    }
+}
--- Assets/Scripts/Cards.cs
+++ Assets/Scripts/Cards.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 [CreateAssetMenu(menuName = "Cards")]
@@ -9,4 +10,14 @@ public class Cards : ScriptableObject
 
     public int cNumber;
     public string cName;
+    public string upright;
+    public string downright;
+    public Sprite CardImg;
+
+    public Cards card;
+
+    private void OnEnable()
+    {
+        
+    }
 }
